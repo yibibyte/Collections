cfef4856df360ce4886633b74c60d011c1327e9e
yibibyte <gnia@artcollege.ru>
2024-12-16 01:47:37 +0300

    add gui form for app

---
+++ src/main/java/ru/java/collections/GUIApp.java
@@ -0,0 +1,16 @@
+package ru.java.collections;
+
+import javax.swing.*;
+
+public class GUIApp {
+    private JLabel label1;
+    private JButton button1;
+    private JPasswordField passwordField1;
+    private JTextPane textPane1;
+    private JTree tree1;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
