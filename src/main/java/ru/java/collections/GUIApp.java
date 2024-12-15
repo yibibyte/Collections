e407e98e669302634066ad3a1870dd07e95c2f27
yibibyte <gnia@artcollege.ru>
2024-12-16 01:49:16 +0300

    add listener mouse

--- src/main/java/ru/java/collections/GUIApp.java
+++ src/main/java/ru/java/collections/GUIApp.java
@@ -1,6 +1,9 @@
 package ru.java.collections;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 public class GUIApp {
     private JLabel label1;
@@ -9,6 +12,50 @@ public class GUIApp {
     private JTextPane textPane1;
     private JTree tree1;
     private JPanel panel1;
+
+    public GUIApp() {
+        panel1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                super.mouseReleased(e);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                super.mouseEntered(e);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                super.mouseExited(e);
+            }
+
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                super.mouseWheelMoved(e);
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                super.mouseDragged(e);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                super.mouseMoved(e);
+            }
+        });
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
