251c3a5d84c2a0c3ff51a6447241f60f8cfc172e
yibibyte <gnia@artcollege.ru>
2024-12-19 18:59:48 +0300

    add dialog windows

--- src/main/java/ru/java/collections/swing/RegistrationApp.java
+++ src/main/java/ru/java/collections/swing/RegistrationApp.java
@@ -1,6 +1,10 @@
 package ru.java.collections.swing;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 
 public class RegistrationApp extends JDialog{
     private JButton resistraionButton;
@@ -9,12 +13,25 @@ public class RegistrationApp extends JDialog{
     private JTextField textField2;
     private JPanel RegistrationPanel;
     RegistrationApp(){
-        setTitle("Registration");
+        setTitle("Регистрация пользователя");
         setContentPane(RegistrationPanel);
         setSize(600, 300);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(RegistrationPanel, "Вы успешно отменили регистрацию пользователя");
+            }
+        });
+        resistraionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(RegistrationPanel, "Вы успешно зарегистрировались");
+            }
+        });
     }
+
     public static void main(String[] args) {
         new RegistrationApp();
 
